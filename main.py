fd525a36b332ae4be031e1bbdfd8ff6c7b5abeaa
Schnuisen <louis9810@aol.com>
2026-09-02 08:55:06 +0200

    Upade functions implemented

--- main.py
+++ main.py
@@ -9,8 +9,31 @@ def search_ticket(searched_id):
 
     for ticket in ticket_list:
         if ticket.ticket_id == searched_id:
-            print(ticket)
             return ticket
+    return None
+
+def update_status(ticket_id,new_status):
+    ticket = search_ticket(ticket_id)
+    if ticket is not None:
+        ticket.status = new_status
+        return ticket
+    
+    return None
+
+def update_priority(ticket_id, new_priority):
+    ticket = search_ticket(ticket_id)
+    if ticket is not None:
+        ticket.priority = new_priority
+        return ticket
+    
+    return None
+
+def update_category(ticket_id, new_category):
+    ticket = search_ticket(ticket_id)
+    if ticket is not None:
+        ticket.category = new_category
+        return ticket
+    
     return None
 
 ticket1 = Ticket(1,"Drucker kaputt", "Drucker schluckt das papier. Komplett verstopft", Status.PENDING, Priority.CRITICAL, Category.SOFTWARE)
@@ -20,6 +43,7 @@ ticket_list.append(ticket1)
 ticket_list.append(ticket2)
 
 
-result = search_ticket(2)
-print(result.title)
-print(result.category)
+
+print(ticket2)
+update_category(2,Category.SOFTWARE)
+print(ticket2)
