ade0d5a67b6592ab3b3d3737c5185e065fd1dea9
Schnuisen <louis9810@aol.com>
2026-09-02 10:54:40 +0200

    CRUD-System finished

--- main.py
+++ main.py
@@ -5,13 +5,49 @@ from category import Category
 
 ticket_list = []
 
+###CREATE / GENERATE###
+def generate_ticket_id():
+
+    if ticket_list != []:
+        ticket = ticket_list[-1]
+        generated_id = ticket.ticket_id +1
+        print(generated_id)
+        return generated_id
+    else:
+        generated_id = 1
+        print(generated_id)
+        return generated_id
+
+def create_ticket(title,description,status,priority,category):
+
+    generated_id = generate_ticket_id()
+    new_ticket = Ticket(generated_id,title,description,status,priority,category)
+
+    ticket_list.append(new_ticket)
+    return new_ticket
+###################################
+
+###DELTE###
+def delete_ticket(ticket_id):
+    ticket = search_ticket(ticket_id)
+
+    if ticket is not None:
+        ticket_list.remove(ticket)
+        return ticket
+
+    return None
+
+
+###SEARCH FUNCTION
 def search_ticket(searched_id):
 
     for ticket in ticket_list:
         if ticket.ticket_id == searched_id:
             return ticket
     return None
+###################################
 
+###UPDATE FUNCTIONS###
 def update_status(ticket_id,new_status):
     ticket = search_ticket(ticket_id)
     if ticket is not None:
@@ -35,15 +71,15 @@ def update_category(ticket_id, new_category):
         return ticket
     
     return None
-
-ticket1 = Ticket(1,"Drucker kaputt", "Drucker schluckt das papier. Komplett verstopft", Status.PENDING, Priority.CRITICAL, Category.SOFTWARE)
-ticket2 = Ticket(2,"Computer stürzt ab", "Computer stürzt direkt nach dem Starten ab",Status.PENDING, Priority.CRITICAL, Category.HARDWARE)
-
-ticket_list.append(ticket1)
-ticket_list.append(ticket2)
+###################################
 
 
 
+
+
+ticket1 = create_ticket("Computer stürzt ab", "Nach hochfahren stürzt der Computer sofort ab", Status.PENDING, Priority.HIGH, Category.HARDWARE)
+print(ticket1)
+ticket2 = create_ticket("Casddasd", "Nasasfasdasdb", Status.IN_PROGRESS, Priority.LOW, Category.SERVER)
 print(ticket2)
-update_category(2,Category.SOFTWARE)
-print(ticket2)
+
+delete_ticket(2)
